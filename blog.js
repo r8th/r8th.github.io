ba0839fbb6628731288351ba1f06d8c00ef4ff6b
Ryan OGrady <17rogrady@gmail.com>
2026-06-22 10:50:26 -0400

    FINAL FINAL V2V2

--- blog.js
+++ blog.js
@@ -1,13 +1,13 @@
 function generateBlogs(p) {
     blogHTMLOutput = ""
     p.blogs.forEach((bObj, bIdx) => {
-        
+
         blogHTMLOutput += `<div class='pcg-section'>`
         let num = bIdx + 1
         num = num.toString().padStart(2, '0')
-        blogHTMLOutput +=  `<p class='pcg-section-num'>${num} — ${bObj.brief}</p>`
+        blogHTMLOutput += `<p class='pcg-section-num'>${num} — ${bObj.brief}</p>`
         blogHTMLOutput += `<h2 class="pcg-section-heading">${bObj.heading}</h2>`
-        
+
         if (typeof bObj.body === "string") {
             blogHTMLOutput += `<p class="pcg-section-body">${bObj.body}</p>`
         } else {
@@ -15,16 +15,14 @@ function generateBlogs(p) {
                 blogHTMLOutput += `<p class="pcg-section-body">${paragraph}</p>`
             });
         }
-        
+
         bObj.images.forEach((imgIdx) => {
             if (typeof imgIdx === "number") {
                 blogHTMLOutput += `<div class="pcg-img-block">`
-                if(p.images[imgIdx].type == "video")
-                {
+                if (p.images[imgIdx].type == "video") {
                     blogHTMLOutput += `<video controls playsinline><source src="${p.images[imgIdx].src}" type="video/mp4"></video>`
                 }
-                else
-                {
+                else {
                     blogHTMLOutput += `<img src="${p.images[imgIdx].src}" alt="${p.images[imgIdx].alt}">`
                 }
                 blogHTMLOutput += `</div>`
@@ -32,7 +30,12 @@ function generateBlogs(p) {
             else {
                 blogHTMLOutput += `<div class="pcg-img-row" style="grid-template-columns: repeat(${imgIdx.length}, 1fr);">`
                 imgIdx.forEach((imgIdx2) => {
-                    blogHTMLOutput += `<img src="${p.images[imgIdx2].src}" alt="${p.images[imgIdx2].alt}">`
+                    if (p.images[imgIdx2].type == "video") {
+                        blogHTMLOutput += `<video controls playsinline><source src="${p.images[imgIdx2].src}" type="video/mp4"></video>`
+                    }
+                    else {
+                        blogHTMLOutput += `<img src="${p.images[imgIdx2].src}" alt="${p.images[imgIdx2].alt}">`
+                    }
                 });
                 blogHTMLOutput += `</div>`
             }
@@ -49,9 +52,8 @@ function generateBlogs(p) {
         refHTML += `<span>${element.text}</span><span>↗</span>`
     });
     refHTML += "</div></div>"
-    
-    if(useRef)
-    {
+
+    if (useRef) {
         blogHTMLOutput += refHTML
     }
 
